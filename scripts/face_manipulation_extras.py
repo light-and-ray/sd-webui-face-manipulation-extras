257efc1d953b8aaa967b3d3552f05ac384608878
Andray <light.and.ray@gmail.com>
2024-07-13 16:51:51 +0400

    Add all factors

--- scripts/face_manipulation_extras.py
+++ scripts/face_manipulation_extras.py
@@ -3,7 +3,12 @@ from modules import scripts_postprocessing
 from modules.ui_components import InputAccordion
 from face_manipulation.main import process, alignImage
 
-
+AGE = ["kid", "teen", "adult", "old"]
+GENDER = ["male", "female"]
+ETHNICITY = ["black", "white", "asian"]
+HAIR = ["brunette", "blond", "bald", "red", "black", "white"]
+BEARD = ["beard", "mustache", "goatee", "shaved"]
+GLASSES = ["glasses", "shades", "no glasses"]
 
 class FaceManipulationExtras(scripts_postprocessing.ScriptPostprocessing):
     name = "Face manipulation"
@@ -15,13 +20,41 @@ class FaceManipulationExtras(scripts_postprocessing.ScriptPostprocessing):
             selectedFactor = gr.Textbox(visible=False, value="age")
             with gr.Tabs():
                 with gr.Tab("Age") as age:
-                    ageChoice = gr.Radio(value="all", choices=["all", "kid", "teen", "adult", "old"], type="index", label="")
+                    ageChoice = gr.Radio(value="all", choices=["all"]+AGE, label="")
+
+                with gr.Tab("Gender") as gender:
+                    genderChoice = gr.Radio(value="all", choices=["all"]+GENDER, label="")
+
+                with gr.Tab("Ethnicity") as ethnicity:
+                    ethnicityChoice = gr.Radio(value="all", choices=["all"]+ETHNICITY, label="")
+
+                with gr.Tab("Hair") as hair:
+                    hairChoice = gr.Radio(value="all", choices=["all"]+HAIR, label="")
+
+                with gr.Tab("Facial hair") as beard:
+                    beardChoice = gr.Radio(value="all", choices=["all"]+BEARD, label="")
+
+                with gr.Tab("Glasses") as glasses:
+                    glassesChoice = gr.Radio(value="all", choices=["all"]+GLASSES, label="")
+
             includeOriginal = gr.Checkbox(value=True, label="Include aligned original")
+
         age.select(fn=lambda: "age", inputs=[], outputs=[selectedFactor])
+        gender.select(fn=lambda: "gender", inputs=[], outputs=[selectedFactor])
+        ethnicity.select(fn=lambda: "ethnicity", inputs=[], outputs=[selectedFactor])
+        hair.select(fn=lambda: "hair_color", inputs=[], outputs=[selectedFactor])
+        beard.select(fn=lambda: "beard", inputs=[], outputs=[selectedFactor])
+        glasses.select(fn=lambda: "glasses", inputs=[], outputs=[selectedFactor])
+
         args = {
             'enable': enable,
             'selectedFactor': selectedFactor,
             'ageChoice': ageChoice,
+            'genderChoice': genderChoice,
+            'ethnicityChoice': ethnicityChoice,
+            'hairChoice': hairChoice,
+            'beardChoice': beardChoice,
+            'glassesChoice': glassesChoice,
             'includeOriginal': includeOriginal,
         }
         return args
@@ -33,17 +66,37 @@ class FaceManipulationExtras(scripts_postprocessing.ScriptPostprocessing):
         results = []
 
         factor = args['selectedFactor'].lower()
-        choice = 0
+
+        choices = None
+        choice = None
         if factor == 'age':
             choice = args['ageChoice']
+            choices = AGE
+        elif factor == 'gender':
+            choice = args['genderChoice']
+            choices = GENDER
+        elif factor == 'ethnicity':
+            choice = args['ethnicityChoice']
+            choices = ETHNICITY
+        elif factor == 'hair_color':
+            choice = args['hairChoice']
+            choices = HAIR
+        elif factor == 'beard':
+            choice = args['beardChoice']
+            choices = BEARD
+        elif factor == 'glasses':
+            choice = args['glassesChoice']
+            choices = GLASSES
+
+
         index = None
-        if choice > 0:
-            index = choice - 1
+        if choice != "all":
+            index = choices.index(choice)
 
         for face in alignImage(pp.image):
             if args['includeOriginal']:
                 results.append(face)
-            processed = process(face, 'age', index)
+            processed = process(face, factor, index)
             results.extend(processed)
 
         if not results:
@@ -54,5 +107,5 @@ class FaceManipulationExtras(scripts_postprocessing.ScriptPostprocessing):
         if len(results) > 1:
             pp.extra_images.extend(results[1:])
 
-        pp.info[self.name] = "age: all"
+        pp.info[self.name] = f"{factor}: {choice}"
 
